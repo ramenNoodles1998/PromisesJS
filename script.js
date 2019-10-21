24182efe3beddc4c900c46ff933ebec8680f7086
Roman Meredith <romanmeredith@gmail.com>
2019-10-21 15:06:35 -0600

    styles.css and script.js made

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-let giphyApi = "https://api.giphy.com/v1/gifs/random?api_key=llhKshOrFlGFsQcpOaaeiv37SEQmata7&tag=&rating=G"
+
+var img;
 function setup(){
+    document.getElementById("btn").addEventListener('click', function(){
+        let input = document.getElementById("text").value;
+        let giphyApi = `https://api.giphy.com/v1/gifs/search?api_key=llhKshOrFlGFsQcpOaaeiv37SEQmata7&q=${input}&limit=25&offset=0&rating=G&lang=en`
+        fetch(giphyApi)
+        .then((response)=>{
+            return response.json();
+        }).then(json=>{
+            createImg(json.data[0].images['fixed_height_small'].url);
+        
+        })
+        .catch((err)=>console.log(err))
+    })
+
     noCanvas();
-    let promise = fetch(giphyApi);
-    console.log(promise);
+    
+   
 }
